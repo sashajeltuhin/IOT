b4528a363ba5cc7196dc1999952c12e9a828b3b8
Sasha <sjeltuhin@apprenda.com>
2016-03-15 15:53:48 -0400

    tests first revision

---
+++ tests/sensor.js
@@ -0,0 +1,19 @@
+var chai = require("chai");
+var chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+describe("Sensor Update Tests", function(){
+    it('Check status', function(done){
+        var should = chai.should();
+        chai.request('http://localhost:8033')
+            .get('/sensor/update/334/12')
+            .end(function(err, res){
+                if (!err) {
+                    res.should.have.status(200);
+                }
+                else{
+                    should.not.exist(err);
+                }
+                done();
+            });
+    });
+});
