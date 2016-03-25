a8bb2078fe8bdb06f696ec861c971d909a17e1d8
Sasha <sjeltuhin@apprenda.com>
2016-03-25 09:27:34 -0400

    added sensor threshold

--- routes/sensor.js
+++ routes/sensor.js
@@ -1,7 +1,8 @@
-
+var threshold = 50;
 exports.update = function(req, res, next){
     var sensorID = req.params.sid;
     var sensorVal = req.params.val;
     var date = new Date();
-    res.send({sid: sensorID, date: date});
+    var st = Number(sensorVal) < threshold ? "Normal" : "Elevated";
+    res.send({sid: sensorID, date: date, status: st});
 }
